15c32fdeb4d052e781cb2005fbe27e3e2b1b58d7
Brian W. Wolter <brian@brianwolter.com>
2016-08-05 16:34:29 -0400

    ...

--- src/main/main.go
+++ src/main/main.go
@@ -20,7 +20,6 @@ func main() {
   cmdline       := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
   fBaseURL      := cmdline.String   ("base-url",        coalesce(os.Getenv("HUNIT_BASE_URL"), "http://localhost/"),   "The base URL for requests.")
   fTrimEntity   := cmdline.Bool     ("entity:trim",     true,                                                         "Trim trailing whitespace from entities.")
-  // fParseEntity  := cmdline.Bool     ("entity:semantic", true,                                                         "Parse supported entities and compare them semantically.")
   fDebug        := cmdline.Bool     ("debug",           false,                                                        "Enable debugging mode.")
   fVerbose      := cmdline.Bool     ("verbose",         false,                                                        "Enable verbose debugging mode.")
   cmdline.Parse(os.Args[1:])
@@ -32,9 +31,6 @@ func main() {
   if *fTrimEntity {
     options |= hunit.OptionEntityTrimTrailingWhitespace
   }
-  // if *fParseEntity {
-  //   options |= hunit.OptionEntityCompareSemantically
-  // }
   
   success := true
   for _, e := range cmdline.Args() {
