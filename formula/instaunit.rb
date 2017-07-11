4dc37d226e3d08b871e541651cd4552fde8340e9
Brian W. Wolter <brian@brianwolter.com>
2017-07-10 23:08:15 -0400

    Actually fixing Homebrew formula

--- formula/instaunit.rb
+++ formula/instaunit.rb
@@ -1,11 +1,11 @@
 
 class Instaunit < Formula
   homepage "https://github.com/instaunit/instaunit"
-  url "https://github.com/instaunit/instaunit/releases/download/v1.0.2/instaunit-v1.0.2-darwin-amd64.tgz"
-  sha256 "c9ccd87f13d804d7367a6ff3129258152a4db7cff0a2d99da59ea1b2ce6ad4ce"
-  version "v1.0.2"
+  url "https://github.com/instaunit/instaunit/releases/download/v1.0.3/instaunit-v1.0.3-darwin-amd64.tgz"
+  sha256 "ad42ac112cd5f45f5f266d557b4aaab54f7688a9d818bf99299446c8d86e02ce"
+  version "v1.0.3"
   
   def install
-    system "make", "install", "PREFIX=#{prefix}"
+    system "install", "-d", "-m", "0755", "bin", "#{prefix}/bin"
   end
 end
