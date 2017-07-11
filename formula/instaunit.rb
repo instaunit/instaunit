cc269211ac348cf40c5f1d3e04c1c6c8b91d1e14
Brian W. Wolter <brian@brianwolter.com>
2017-07-10 22:49:11 -0400

    Formula updates

--- formula/instaunit.rb
+++ formula/instaunit.rb
@@ -3,6 +3,7 @@ class Instaunit < Formula
   homepage "https://github.com/instaunit/instaunit"
   url "https://github.com/instaunit/instaunit/releases/download/v1.0.1/instaunit-v1.0.1-darwin-amd64.tgz"
   sha256 "6ab14f02e5a19663894451d88c2ba69c55623755d1937aae0f003dc8b1763e35"
+  version "v1.0.1"
   
   def install
     system "make", "install", "PREFIX=#{prefix}"
