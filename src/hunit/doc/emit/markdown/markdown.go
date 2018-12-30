17b4aaf5604429055c68217e324197ce96f60b98
Brian W. Wolter <brian@brianwolter.com>
2018-12-30 16:43:09 -0500

    Adding support for comments in request and response
    And customization of the request and response headers.

--- src/hunit/doc/emit/markdown/markdown.go
+++ src/hunit/doc/emit/markdown/markdown.go
@@ -1,250 +1,270 @@
 package markdown
 
 import (
-  "io"
-  "fmt"
-  "sort"
-  "bytes"
-  "strings"
-  "net/http"
-  "hunit/test"
-  "hunit/text"
-  "hunit/text/slug"
+	"bytes"
+	"fmt"
+	"hunit/test"
+	"hunit/text"
+	"hunit/text/slug"
+	"io"
+	"net/http"
+	"sort"
+	"strings"
 )
 
 /**
  * A markdown documentation generator
  */
 type Generator struct {
-  w         io.Writer
-  b         *bytes.Buffer
-  sections  map[string]string
-  slugs     map[string]int
-  
+	w        io.Writer
+	b        *bytes.Buffer
+	sections map[string]string
+	slugs    map[string]int
 }
 
 /**
  * Produce a new emitter
  */
 func New(w io.Writer) *Generator {
-  return &Generator{w, nil, make(map[string]string), nil}
+	return &Generator{w, nil, make(map[string]string), nil}
 }
 
 /**
  * Init a suite
  */
 func (g *Generator) Init(suite *test.Suite) error {
-  g.b = &bytes.Buffer{}
-  return nil
+	g.b = &bytes.Buffer{}
+	return nil
 }
 
 /**
  * Finish a suite
  */
 func (g *Generator) Finalize(suite *test.Suite) error {
-  var err error
-  
-  err = g.prefix(g.w, suite)
-  if err != nil {
-    return err
-  }
-  
-  err = g.contents(g.w, suite)
-  if err != nil {
-    return err
-  }
-  
-  _, err = g.w.Write(g.b.Bytes())
-  if err != nil {
-    return err
-  }
-  
-  return nil
+	var err error
+
+	err = g.prefix(g.w, suite)
+	if err != nil {
+		return err
+	}
+
+	err = g.contents(g.w, suite)
+	if err != nil {
+		return err
+	}
+
+	_, err = g.w.Write(g.b.Bytes())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 /**
  * Generate documentation
  */
 func (g *Generator) Case(conf test.Config, c test.Case, req *http.Request, reqdata string, rsp *http.Response, rspdata []byte) error {
-  return g.generate(g.b, conf, c, req, reqdata, rsp, rspdata)
+	return g.generate(g.b, conf, c, req, reqdata, rsp, rspdata)
 }
 
 /**
  * Generate documentation preamble
  */
 func (g *Generator) prefix(w io.Writer, suite *test.Suite) error {
-  var err error
-  var doc string
-  
-  if suite.Title != "" {
-    doc += fmt.Sprintf("# %s\n\n", strings.TrimSpace(suite.Title))
-  }
-  if suite.Comments != "" {
-    doc += strings.TrimSpace(suite.Comments) +"\n\n"
-  }
-  
-  _, err = fmt.Fprint(w, doc)
-  if err != nil {
-    return err
-  }
-  
-  return nil
+	var err error
+	var doc string
+
+	if suite.Title != "" {
+		doc += fmt.Sprintf("# %s\n\n", strings.TrimSpace(suite.Title))
+	}
+	if suite.Comments != "" {
+		doc += strings.TrimSpace(suite.Comments) + "\n\n"
+	}
+
+	_, err = fmt.Fprint(w, doc)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 /**
  * Table of contents
  */
 func (g *Generator) contents(w io.Writer, suite *test.Suite) error {
-  var err error
-  var doc string
-  
-  doc += "## Contents\n\n"
-  
-  for s, t := range g.sections {
-    doc += fmt.Sprintf("* [%s](#%s)\n", strings.TrimSpace(t), s)
-  }
+	var err error
+	var doc string
 
-  doc += "\n"
-  
-  _, err = fmt.Fprint(w, doc)
-  if err != nil {
-    return err
-  }
-  
-  return nil
+	doc += "## Contents\n\n"
+
+	for s, t := range g.sections {
+		doc += fmt.Sprintf("* [%s](#%s)\n", strings.TrimSpace(t), s)
+	}
+
+	doc += "\n"
+
+	_, err = fmt.Fprint(w, doc)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 /**
  * Generate documentation
  */
 func (g *Generator) generate(w io.Writer, conf test.Config, c test.Case, req *http.Request, reqdata string, rsp *http.Response, rspdata []byte) error {
-  var err error
-  var doc string
-  
-  var t string
-  if c.Title != "" {
-    t = strings.TrimSpace(c.Title)
-  }else{
-    t = fmt.Sprintf("%s %s", c.Request.Method, c.Request.URL)
-  }
-  
-  doc += fmt.Sprintf("## %s\n\n", t)
-  var s string
-  s, g.slugs = slug.Github(t, g.slugs)
-  g.sections[s] = t
-  
-  if c.Comments != "" {
-    doc += strings.TrimSpace(c.Comments) +"\n\n"
-  }
-  
-  if c.Params != nil {
-    types, maxkey, maxtype := false, 5, 5
-    var tmap map[string]string
-    
-    params := make(map[string]string)
-    keys := make([]string, len(c.Params))
-    i := 0
-    for k, _ := range c.Params {
-      keys[i] = k; i++
-    }
-    sort.Strings(keys)
-    
-    for k, v := range c.Params {
-      t := strings.TrimSpace(k)
-      v  = strings.TrimSpace(v)
-      if l := len(t); l > maxkey {
-        maxkey = l
-      }
-      if v[0] == '`' {
-        types = true
-        x := strings.Index(v[1:], "`") + 1
-        if x > maxtype {
-          maxtype = x
-        }
-        if tmap == nil {
-          tmap = make(map[string]string)
-        }
-        tmap[k] = v[:x+1]
-        params[k] = v[x+1:]
-      }else{
-        params[k] = v
-      }
-    }
-    
-    doc += "### Query Parameters\n"
-    
-    var f string
-    if types {
-      doc += "| Param | Type | Detail |\n"
-      doc += "|-------|------|--------|\n"
-      f = fmt.Sprintf("| %%%ds | %%%ds | %%5s |\n", maxkey, maxtype)
-    }else{
-      doc += "| Param | Detail |\n"
-      doc += "|-------|--------|\n"
-      f = fmt.Sprintf("| %%%ds | %%5s |\n", maxkey)
-    }
-    
-    for _, k := range keys {
-      t := strings.TrimSpace(k)
-      v := strings.TrimSpace(params[k])
-      if types {
-        doc += fmt.Sprintf(f, "`"+ t +"`", tmap[k], v)
-      }else{
-        doc += fmt.Sprintf(f, "`"+ t +"`", v)
-      }
-    }
-  }
-  
-  if req != nil {
-    b := &bytes.Buffer{}
-    if len(reqdata) > 0 && conf.Doc.FormatEntities {
-      t := text.Coalesce(c.Request.Format, req.Header.Get("Content-Type"))
-      f, err := text.FormatEntity([]byte(reqdata), t)
-      if err == nil {
-        reqdata = string(f)
-      }else if err != nil && err != text.ErrUnsupportedContentType {
-        fmt.Println("* * * Invalid request entity could not be formatted: %v", t)
-      }
-    }
-    err = text.WriteRequest(b, req, reqdata)
-    if err != nil {
-      return err
-    }
-    if b.Len() > 0 {
-      doc += "### Example request\n\n"
-      doc += "```http\n"
-      doc += string(b.Bytes()) +"\n"
-      doc += "```\n"
-    }
-  }
-  
-  if rsp != nil {
-    b := &bytes.Buffer{}
-    if len(rspdata) > 0 && conf.Doc.FormatEntities {
-      t := text.Coalesce(c.Response.Format, rsp.Header.Get("Content-Type"))
-      f, err := text.FormatEntity(rspdata, t)
-      if err == nil {
-        rspdata = f
-      }else if err != nil && err != text.ErrUnsupportedContentType {
-        fmt.Println("* * * Invalid entity could not be formatted: %v", t)
-      }
-    }
-    err = text.WriteResponse(b, rsp, rspdata)
-    if err != nil {
-      return err
-    }
-    if b.Len() > 0 {
-      doc += "### Example response\n\n"
-      doc += "```http\n"
-      doc += string(b.Bytes()) +"\n"
-      doc += "```\n"
-    }
-  }
-  
-  _, err = fmt.Fprint(g.b, doc +"\n\n")
-  if err != nil {
-    return err
-  }
-  
-  return nil
+	var err error
+	var doc string
+
+	var t string
+	if c.Title != "" {
+		t = strings.TrimSpace(c.Title)
+	} else {
+		t = fmt.Sprintf("%s %s", c.Request.Method, c.Request.URL)
+	}
+
+	doc += fmt.Sprintf("## %s\n\n", t)
+	var s string
+	s, g.slugs = slug.Github(t, g.slugs)
+	g.sections[s] = t
+
+	if c.Comments != "" {
+		doc += strings.TrimSpace(c.Comments) + "\n\n"
+	}
+
+	if c.Params != nil {
+		types, maxkey, maxtype := false, 5, 5
+		var tmap map[string]string
+
+		params := make(map[string]string)
+		keys := make([]string, len(c.Params))
+		i := 0
+		for k, _ := range c.Params {
+			keys[i] = k
+			i++
+		}
+		sort.Strings(keys)
+
+		for k, v := range c.Params {
+			t := strings.TrimSpace(k)
+			v = strings.TrimSpace(v)
+			if l := len(t); l > maxkey {
+				maxkey = l
+			}
+			if v[0] == '`' {
+				types = true
+				x := strings.Index(v[1:], "`") + 1
+				if x > maxtype {
+					maxtype = x
+				}
+				if tmap == nil {
+					tmap = make(map[string]string)
+				}
+				tmap[k] = v[:x+1]
+				params[k] = v[x+1:]
+			} else {
+				params[k] = v
+			}
+		}
+
+		doc += "### Query Parameters\n\n"
+
+		var f string
+		if types {
+			doc += "| Param | Type | Detail |\n"
+			doc += "|-------|------|--------|\n"
+			f = fmt.Sprintf("| %%%ds | %%%ds | %%5s |\n", maxkey, maxtype)
+		} else {
+			doc += "| Param | Detail |\n"
+			doc += "|-------|--------|\n"
+			f = fmt.Sprintf("| %%%ds | %%5s |\n", maxkey)
+		}
+
+		for _, k := range keys {
+			t := strings.TrimSpace(k)
+			v := strings.TrimSpace(params[k])
+			if types {
+				doc += fmt.Sprintf(f, "`"+t+"`", tmap[k], v)
+			} else {
+				doc += fmt.Sprintf(f, "`"+t+"`", v)
+			}
+		}
+
+		doc += "\n\n"
+	}
+
+	if req != nil {
+		b := &bytes.Buffer{}
+		if len(reqdata) > 0 && conf.Doc.FormatEntities {
+			t := text.Coalesce(c.Request.Format, req.Header.Get("Content-Type"))
+			f, err := text.FormatEntity([]byte(reqdata), t)
+			if err == nil {
+				reqdata = string(f)
+			} else if err != nil && err != text.ErrUnsupportedContentType {
+				fmt.Println("* * * Invalid request entity could not be formatted: %v", t)
+			}
+		}
+		err = text.WriteRequest(b, req, reqdata)
+		if err != nil {
+			return err
+		}
+		if c.Request.Title != "" {
+			doc += fmt.Sprintf("### %s\n\n", strings.TrimSpace(c.Request.Title))
+		} else if b.Len() > 0 {
+			doc += "### Example request\n\n"
+		} else if c.Request.Comments != "" {
+			doc += "### Request\n\n"
+		}
+		if c.Request.Comments != "" {
+			doc += strings.TrimSpace(c.Request.Comments) + "\n\n"
+		}
+		if b.Len() > 0 {
+			doc += "```http\n"
+			doc += string(b.Bytes()) + "\n"
+			doc += "```\n\n"
+		}
+	}
+
+	if rsp != nil {
+		b := &bytes.Buffer{}
+		if len(rspdata) > 0 && conf.Doc.FormatEntities {
+			t := text.Coalesce(c.Response.Format, rsp.Header.Get("Content-Type"))
+			f, err := text.FormatEntity(rspdata, t)
+			if err == nil {
+				rspdata = f
+			} else if err != nil && err != text.ErrUnsupportedContentType {
+				fmt.Println("* * * Invalid entity could not be formatted: %v", t)
+			}
+		}
+		err = text.WriteResponse(b, rsp, rspdata)
+		if err != nil {
+			return err
+		}
+		if c.Response.Title != "" {
+			doc += fmt.Sprintf("### %s\n\n", strings.TrimSpace(c.Response.Title))
+		} else if b.Len() > 0 {
+			doc += "### Example response\n\n"
+		} else if c.Response.Comments != "" {
+			doc += "### Response\n\n"
+		}
+		if c.Response.Comments != "" {
+			doc += strings.TrimSpace(c.Response.Comments) + "\n\n"
+		}
+		if b.Len() > 0 {
+			doc += "```http\n"
+			doc += string(b.Bytes()) + "\n"
+			doc += "```\n\n"
+		}
+	}
+
+	_, err = fmt.Fprint(g.b, doc+"\n")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- src/hunit/test/test.go
+++ src/hunit/test/test.go
@@ -1,82 +1,86 @@
 package test
 
 import (
-  "time"
+	"time"
 )
 
 import (
-  "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
 )
 
 // Options
 type Options uint32
 
 const (
-  OptionNone                          = 0
-  OptionDebug                         = 1 << 0
-  OptionEntityTrimTrailingWhitespace  = 1 << 1
-  OptionInterpolateVariables          = 1 << 2
-  OptionDisplayRequests               = 1 << 3
-  OptionDisplayResponses              = 1 << 4
-  OptionDisplayRequestsOnFailure      = 1 << 5
-  OptionDisplayResponsesOnFailure     = 1 << 6
+	OptionNone                         = 0
+	OptionDebug                        = 1 << 0
+	OptionEntityTrimTrailingWhitespace = 1 << 1
+	OptionInterpolateVariables         = 1 << 2
+	OptionDisplayRequests              = 1 << 3
+	OptionDisplayResponses             = 1 << 4
+	OptionDisplayRequestsOnFailure     = 1 << 5
+	OptionDisplayResponsesOnFailure    = 1 << 6
 )
 
 // Basic credentials
 type BasicCredentials struct {
-  Username    string              `yaml:"username"`
-  Password    string              `yaml:"password"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
 }
 
 // A test request
 type Request struct {
-  Method      string              `yaml:"method"`
-  URL         string              `yaml:"url"`
-  Headers     map[string]string   `yaml:"headers"`
-  Params      map[string]string   `yaml:"params"`
-  Entity      string              `yaml:"entity"`
-  Format      string              `yaml:"format"`
-  BasicAuth   *BasicCredentials   `yaml:"basic-auth"`
+	Method    string            `yaml:"method"`
+	URL       string            `yaml:"url"`
+	Headers   map[string]string `yaml:"headers"`
+	Params    map[string]string `yaml:"params"`
+	Entity    string            `yaml:"entity"`
+	Format    string            `yaml:"format"`
+	BasicAuth *BasicCredentials `yaml:"basic-auth"`
+	Title     string            `yaml:"title"`
+	Comments  string            `yaml:"doc"`
 }
 
 // A test response
 type Response struct {
-  Status      int                 `yaml:"status"`
-  Headers     map[string]string   `yaml:"headers"`
-  Entity      string              `yaml:"entity"`
-  Comparison  Comparison          `yaml:"compare"`
-  Format      string              `yaml:"format"`
+	Status     int               `yaml:"status"`
+	Headers    map[string]string `yaml:"headers"`
+	Entity     string            `yaml:"entity"`
+	Comparison Comparison        `yaml:"compare"`
+	Format     string            `yaml:"format"`
+	Title      string            `yaml:"title"`
+	Comments   string            `yaml:"doc"`
 }
 
 // A connection message stream
 type Stream struct {
-  Mode        IOMode              `yaml:"mode"`
-  Messages    []MessageExchange   `yaml:"messages"`
+	Mode     IOMode            `yaml:"mode"`
+	Messages []MessageExchange `yaml:"messages"`
 }
 
 // A message exchange consisting of zero or one input and zero or one output
 type MessageExchange struct {
-  Wait        time.Duration       `yaml:"wait"`
-  Output      *string             `yaml:"send"`
-  Input       *string             `yaml:"receive"`
+	Wait   time.Duration `yaml:"wait"`
+	Output *string       `yaml:"send"`
+	Input  *string       `yaml:"receive"`
 }
 
 // A test case
 type Case struct {
-  Id        string                `yaml:"id"`
-  Wait      time.Duration         `yaml:"wait"`
-  Repeat    int                   `yaml:"repeat"`
-  Gendoc    bool                  `yaml:"gendoc"`
-  Title     string                `yaml:"title"`
-  Comments  string                `yaml:"doc"`
-  Params    map[string]string     `yaml:"params"`
-  Request   Request               `yaml:"request"`
-  Response  Response              `yaml:"response"`
-  Stream    *Stream               `yaml:"websocket"`
-  Vars      yaml.MapSlice         `yaml:"vars"`
+	Id       string            `yaml:"id"`
+	Wait     time.Duration     `yaml:"wait"`
+	Repeat   int               `yaml:"repeat"`
+	Gendoc   bool              `yaml:"gendoc"`
+	Title    string            `yaml:"title"`
+	Comments string            `yaml:"doc"`
+	Params   map[string]string `yaml:"params"`
+	Request  Request           `yaml:"request"`
+	Response Response          `yaml:"response"`
+	Stream   *Stream           `yaml:"websocket"`
+	Vars     yaml.MapSlice     `yaml:"vars"`
 }
 
 // Determine if this case is documented or not
 func (c Case) Documented() bool {
-  return c.Gendoc || c.Title != "" || c.Comments != ""
+	return c.Gendoc || c.Title != "" || c.Comments != ""
 }
