03b9e42869967c595c5b31e0df56cc0fbaa8fe54
Brian W. Wolter <brian.wolter@bounceexchange.com>
2019-02-28 15:54:39 -0500

    Formula update

--- formula/instaunit.rb
+++ formula/instaunit.rb
@@ -2,7 +2,7 @@
 class Instaunit < Formula
   homepage "https://github.com/instaunit/instaunit"
   url "https://github.com/instaunit/instaunit/releases/download/v1.3.1/instaunit-v1.3.1-darwin-amd64.tgz"
-  sha256 "6c72c2fa339d5f4e5f96edac9c356a7234c30f5e5978eda253cf493fcfa5c893"
+  sha256 "777f97dfd29a5d92dcc75ed92235c39532d61542a1bc0d3193d7df140a13a498"
   version "v1.3.1"
   
   def install
