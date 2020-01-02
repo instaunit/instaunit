fbc0f0c9e610d6d3b39659b4ce04d3fa2c9605cc
Brian W. Wolter <brian.wolter@bounceexchange.com>
2020-01-02 13:24:25 -0500

    Initial script stuff

---
+++ src/hunit/script/script.go
@@ -0,0 +1,78 @@
+package script
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/instaunit/instaunit/hunit/expr"
+
+	"github.com/bww/epl"
+	"github.com/robertkrimen/otto"
+)
+
+type ErrInvalidTypeError struct {
+	source string
+	expect string
+	result interface{}
+}
+
+func (e ErrInvalidTypeError) Error() string {
+	return fmt.Sprintf("Invalid type; expected %s in {%s}, got (%T) %v", e.expect, e.source, e.result, e.result)
+}
+
+// A script
+type Script struct {
+	Type   string `yaml:"type"`
+	Source string `yaml:"source"`
+}
+
+func (s Script) Bool(v expr.Variables) (bool, error) {
+	res, err := s.Eval(v)
+	if err != nil {
+		return false, err
+	}
+	switch v := res.(type) {
+	case bool:
+		return v, nil
+	case otto.Value:
+		return v.ToBoolean()
+	default:
+		return false, ErrInvalidTypeError{s.Source, "bool", res}
+	}
+}
+
+func (s Script) Eval(v expr.Variables) (interface{}, error) {
+	cxt := expr.RuntimeContext(v, os.Environ())
+	switch s.Type {
+	case "epl", "":
+		return s.evalEPL(cxt)
+	case "js", "javascript":
+		return s.evalJS(cxt)
+	default:
+		return false, fmt.Errorf("Unsupported script type: %v", s.Type)
+	}
+}
+
+func (s Script) evalEPL(cxt expr.Variables) (interface{}, error) {
+	prg, err := epl.Compile(s.Source)
+	if err != nil {
+		return false, err
+	}
+	res, err := prg.Exec(cxt)
+	if err != nil {
+		return false, fmt.Errorf("Could not evaluate expression: {%v}: %v", s.Source, err)
+	}
+	return res, nil
+}
+
+func (s Script) evalJS(cxt expr.Variables) (interface{}, error) {
+	vm := otto.New()
+	for k, v := range cxt {
+		vm.Set(k, v)
+	}
+	res, err := vm.Run(s.Source)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
---
+++ src/hunit/script/script_test.go
@@ -0,0 +1,93 @@
+package script
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/instaunit/instaunit/hunit/expr"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	invalidInputError = fmt.Errorf("Invalid input")
+)
+
+var context = expr.Variables{
+	"a": 123,
+	"b": "String value",
+	"c": map[string]interface{}{
+		"a": []string{"Zero", "One", "Two", "Three"},
+		"b": false,
+	},
+}
+
+func TestScripts(t *testing.T) {
+	tests := []struct {
+		Script Script
+		Expect bool
+		Error  error
+	}{
+		{
+			Script{"js", `1 == 2`},
+			false,
+			nil,
+		},
+		{
+			Script{"js", `2 == 2`},
+			true,
+			nil,
+		},
+		{
+			Script{"js", `false`},
+			false,
+			nil,
+		},
+		{
+			Script{"js", `"anything"`},
+			true,
+			nil,
+		},
+		{
+			Script{"js", `c.b`},
+			false,
+			nil,
+		},
+		//
+		{
+			Script{"epl", `1 == 2`},
+			false,
+			nil,
+		},
+		{
+			Script{"epl", `2 == 2`},
+			true,
+			nil,
+		},
+		{
+			Script{"epl", `false`},
+			false,
+			nil,
+		},
+		{
+			Script{"epl", `"Not a bool"`},
+			false,
+			ErrInvalidTypeError{`"Not a bool"`, "bool", "Not a bool"},
+		},
+		{
+			Script{"epl", `c.b`},
+			false,
+			nil,
+		},
+	}
+	for _, e := range tests {
+		fmt.Println(">>>", e.Script)
+		r, err := e.Script.Bool(context)
+		if e.Error != nil {
+			fmt.Println("ERRTIME", err)
+			assert.Equal(t, e.Error, err)
+		} else if assert.Nil(t, err, fmt.Sprint(err)) {
+			fmt.Println("GOTCHA", r)
+			assert.Equal(t, e.Expect, r)
+		}
+	}
+}
